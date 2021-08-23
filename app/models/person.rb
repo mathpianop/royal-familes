69ea0a23fac45767ed09f128ca992b3f2e4bb5df
Paul <mathpianop@gmail.com>
2021-08-23 11:18:10 -0500

    Added validations for Person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,6 @@
 class Person < ApplicationRecord
   has_parents ineligibility: :pedigree_and_dates
+  validates birth_date, unless: -> {birth_date > death_date}
+  validates death_date, unless: -> {birth_date > death_date}
+
 end

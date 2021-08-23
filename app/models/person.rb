edcd7d45fc3766d3f2b86137d28148d3c393cf54
Paul <mathpianop@gmail.com>
2021-08-23 12:12:59 -0500

    Fixed birth before death validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,16 @@
+
+
+
+
 class Person < ApplicationRecord
   has_parents ineligibility: :pedigree_and_dates
-  validates birth_date, unless: -> {birth_date > death_date}
-  validates death_date, unless: -> {birth_date > death_date}
+  has_many :husbanded_marriages, foreign_key: :husband_id, class_name: "Marriage"
+  has_many :wifed_marriages, foreign_key: :wife_id, class_name: "Marriage"
+  has_many :wives, through: :husbanded_marriages, source: :wife
+  has_many :husbands, through: :wifed_marriages, source: :husband
+  validate :birth_must_be_before_death
 
+  def birth_must_be_before_death
+    errors.add(:base, "Birth date must be before death date") if birth_date > death_date
+  end
 end

8fb2074eb73b0f48a8b9a9b9624dd16db36f5d19
Paul <mathpianop@gmail.com>
2021-10-05 21:15:48 -0500

    Switched to custom autocomplete

--- app/javascript/packs/autocomplete.js
+++ app/javascript/packs/autocomplete.js
@@ -1,73 +1,97 @@
-import { autocomplete } from '@algolia/autocomplete-js';
-import '@algolia/autocomplete-theme-classic/dist/theme.css';
-import "styles/custom-autocomplete.css";
+// import { autocomplete } from '@algolia/autocomplete-js';
+// import { html } from 'htm/preact';
+// import '@algolia/autocomplete-theme-classic/dist/theme.css';
+// import "styles/custom-autocomplete.css";
+
+
+
+const suggestions = JSON.parse(document.getElementById('father-search').dataset.suggestions)
 
 const formatSuggestion = function(person) {
   return (person["title"] ? `${person["name"]}, ${person["title"]}` : person["name"])
 }
-const suggestions = JSON.parse(document.getElementById('father-search').dataset.suggestions)
 
+const input = document.getElementById("q");
+input.addEventListener("keyup", (e) => {
+  showMatches(e.target.value)
+})
 
-autocomplete({
-  container: '#father-search',
-  placeholder: 'Search for father...',
-  getSources({ query, setQuery }) {
-    return [
-      {
-        sourceId: "people",
-        getItems() {
-          const text = query.toLowerCase();
-          const people = suggestions;
-          const matches = [];
-          for (let i = 0; i < people.length; i++) {
-              if (~people[i]["name"].toLowerCase().indexOf(text)) {
-                matches.push(people[i])
-              };
-            }
-          return matches
-        },
-        templates: {
-          item({ item }) {
-            const itemButton = document.createElement("BUTTON")
-            itemButton.textContent = formatSuggestion(item)
-            itemButton.addEventListener("click", () => {
-              console.log("Hello")
-              setQuery(formatSuggestion(item))
-            })
-            return formatSuggestion(item)
-          }
-        }
-      }
-    ]
-    
-    
-  },
-});
+const filterMatches = function(query, possiblePeople) {
+  const text = query.toLowerCase();
+  console.log(possiblePeople)
+  return possiblePeople.filter(possiblePerson => {
+    ~possiblePerson["name"].toLowerCase().indexOf(text)
+  })
+}
+
+const showMatches = function(query) {
+  console.log("Hello")
+  results = document.getElementById("results");
+  results.innerHTML = '';
+  let list = '';
+  let terms = filterMatches(query, suggestions);
+  console.log(terms)
+  for (i=0; i<terms.length; i++) {
+    list += '<li>' + terms[i] + '</li>';
+  }
+  results.innerHTML = '<ul>' + list + '</ul>';
+}
 
 
 
-// const autocompleteSearch = function() {
-//   const suggestions = JSON.parse(document.getElementById('father-search-data').dataset.suggestions)
-//   const input = document.getElementById('q');
-  
-//   if (suggestions && input) {
-//     autocomplete({
-//       selector: input,
-//       source: function(text, update) {
-//           text = text.toLowerCase();
+
+
+
+// autocomplete({
+//   container: '#father-search',
+//   placeholder: 'Search for father...',
+//   debug: true,
+//   getSources({ query, setQuery, setIsOpen}) {
+//     const selectItem = function(item) {
+//       setQuery(formatSuggestion(item)); 
+//       setIsOpen(false);
+//     }
+
+//     const handleEnter = function(event) {
+//       if (event.key === "Enter") {
+//         console.log("Yay")
+//       }
+//     }
+
+//     const input = document.getElementsByClassName("aa-Input")[0]
+//     input.addEventListener("keyup", handleEnter)
+//     console.log("Yes")
+
+//     return [
+//       {
+//         sourceId: "people",
+//         getItems() {
+//           const text = query.toLowerCase();
 //           const people = suggestions;
 //           const matches = [];
 //           for (let i = 0; i < people.length; i++) {
 //               if (~people[i]["name"].toLowerCase().indexOf(text)) {
-//                 matches.push(formatSuggestion(people[i]))
+//                 matches.push(people[i])
 //               };
 //             }
-//             console.log(matches)
-//           update(matches);
-//       },
-//     });
-//   }
-// };
+//           return matches
+//         },
+//         templates: {
+//           item({ item }) {
+//             const handleItemClick = function() {
+//               selectItem(item)
+//             }
 
-// const fatherSearchData = document.getElementById('father-search-data')
-// fatherSearchData.addEventListener("keydown", autocompleteSearch)
+           
+//             return html`
+//               <div onClick=${handleItemClick} onKeyUp=${handleEnter}>
+//                 ${formatSuggestion(item)}
+//               </div>
+//           `}
+//         }
+//       }
+//     ]
+    
+    
+//   },
+// });

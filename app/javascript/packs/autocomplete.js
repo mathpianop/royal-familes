fe4b0e6023ac5ce3888a2157c8372ce1700e5cbc
Paul <mathpianop@gmail.com>
2021-10-07 21:44:05 -0500

    Made autocomplete autofill

--- app/javascript/packs/autocomplete.js
+++ app/javascript/packs/autocomplete.js
@@ -1,37 +1,99 @@
 // import { autocomplete } from '@algolia/autocomplete-js';
 // import { html } from 'htm/preact';
 // import '@algolia/autocomplete-theme-classic/dist/theme.css';
-// import "styles/custom-autocomplete.css";
+import "styles/autocomplete.css";
 
 
 
-const suggestions = JSON.parse(document.getElementById('father-search').dataset.suggestions)
+//Abstract
+const sources = JSON.parse(document.getElementById('father-search').dataset.suggestions)
 
-const formatSuggestion = function(person) {
+//Abstract
+const input = document.getElementById("q");
+input.addEventListener("keyup", (e) => {
+  showMatches(e.target.value)
+})
+
+//Abstract
+const filterSources = function(query, sources) {
+  const text = query.toLowerCase();
+  return sources.filter(source => {
+    return ~source["name"].toLowerCase().indexOf(text)
+  }).map(formatMatch)
+}
+
+//Abstract
+const formatMatch = function(person) {
   return (person["title"] ? `${person["name"]}, ${person["title"]}` : person["name"])
 }
 
-const input = document.getElementById("q");
-input.addEventListener("keyup", (e) => {
-  showPeople(e.target.value)
-})
+let matches
+let matchNodes;
+let activeMatchId;
 
-const filterPeople = function(query, possiblePeople) {
-  const text = query.toLowerCase();
-  return possiblePeople.filter(possiblePerson => {
-    return ~possiblePerson["name"].toLowerCase().indexOf(text)
+const markActive = function(matchEl) {
+  activeMatchId = matchEl.dataset.matchIndex;
+  //Fill the input with the text of the match
+  input.value = matchNodes[activeMatchId].innerText;
+  matchEl.classList.add("active")
+}
+
+const markInactive = function(matchEl) {
+  matchEl.classList.remove("active")
+}
+
+const createMatchNodes = function(matches) {
+  return matches.map((match, index) => {
+    const item = document.createElement("LI");
+    item.dataset.matchIndex = index;
+    item.addEventListener("mouseenter", e => markActive(e.target));
+    item.addEventListener("mouseleave", e => markInactive(e.target));
+    typeof match === "string" ? item.textContent = match : item.appendChild(match);
+    return item;
   })
 }
 
-const showPeople = function(query) {
-  results = document.getElementById("results");
-  results.innerHTML = '';
-  const matches = filterPeople(query, suggestions);
-  const list = matches.reduce((acc, match) => {
-    return acc + `<li>${formatSuggestion(match)}</li>`;
-  }, "")
-  results.innerHTML = `<ul>${list}</ul>`;
+document.addEventListener("click", () => {
+  const resultsPanels = document.getElementsByClassName("results");
+  Array.from(resultsPanels).forEach(panel => panel.remove())
+})
+
+
+
+
+
+const showMatches = function(query) {
+  matches = filterSources(query, sources);
+  matchNodes = createMatchNodes(matches)
+  const list = matchNodes.reduce((listParent, matchNode) => {
+    listParent.appendChild(matchNode)
+    return listParent
+  }, document.createElement("UL"))
+  const results = getResultsPanel()
+  results.textContent = "";
+  results.appendChild(list);
 }
+
+
+const getResultsPanel = function() {
+  //If results panel exists, return it. Otherwise, create it.
+  const inputSibling = input.nextElementSibling
+  console.log(inputSibling)
+  if (inputSibling && inputSibling.classList.contains("results")) {
+    return inputSibling
+  } else {
+    return createResultsPanel()
+  }
+}
+
+const createResultsPanel = function() {
+  const resultsPanel = document.createElement("DIV")
+  resultsPanel.classList.add("results")
+  input.insertAdjacentElement("afterend", resultsPanel)
+  return resultsPanel
+}
+
+
 
 
 

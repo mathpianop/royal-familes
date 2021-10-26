d084bc682245994605a6aa9092ed8bbab2928a96
Paul <mathpianop@gmail.com>
2021-10-25 21:39:50 -0500

    Finish initial family tree touchups

--- app/javascript/packs/familyTree.js
+++ app/javascript/packs/familyTree.js
@@ -9,6 +9,14 @@ const onlyMaternalGrandparents = function() {
   )
 }
 
+const onlyPaternalGrandparents = function() {
+  return (
+    grandparentsList &&
+    !grandparentsList.querySelector(".maternal-grandparents") &&
+    grandparentsList.querySelector(".paternal-grandparents")
+  )
+}
+
 const grandparentsWithOnlyOneParent = function() {
   if (parentsList) {
     return (
@@ -19,10 +27,25 @@ const grandparentsWithOnlyOneParent = function() {
   }
 }
 
-if (onlyMaternalGrandparents()) {
-  grandparentsList.classList.add("only-maternal")
+const grandparentsOnOnlyOneSide = function() {
+  return (
+    grandparentsList &&
+    grandparentsList.querySelectorAll("li").length === 1
+  )
 }
+
+
+
+
 
 if (grandparentsWithOnlyOneParent()) {
   grandparentsList.classList.add("grandparents-single-parent")
+} else if (onlyMaternalGrandparents()) {
+  grandparentsList.classList.add("only-maternal")
+} else if (onlyPaternalGrandparents()) {
+  grandparentsList.classList.add("only-paternal")
 }
+
+
+
+

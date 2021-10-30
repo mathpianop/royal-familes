216c266ed43f5f537e1ed4cf47c3851cd9edd767
Paul <mathpianop@gmail.com>
2021-10-29 22:21:59 -0500

    Styled relationship section

--- app/javascript/packs/relationship.js
+++ app/javascript/packs/relationship.js
@@ -24,7 +24,7 @@ relationshipForm.addEventListener("submit", (e) => {
 
 searchPeople({
   container: relationSearch,
-  placeholder: "Search people...",
+  placeholder: "Search relations...",
   onSelect: (selection) => {
     setPersonInfo("relation-id", selection.id);
     getRelationship(relationshipForm.dataset.id);
@@ -37,23 +37,31 @@ const capitalize = function(string) {
   return string[0].toUpperCase() + string.slice(1)
 }
 
-const buildAncestorAnchor = function(name, id) {
+const buildAncestorListItem = function(name, id) {
+  const listItemEl = document.createElement("LI");
   const anchorEl = document.createElement("A");
   anchorEl.textContent = name;
   anchorEl.href = `/people/${id}`;
-  return anchorEl
+  listItemEl.append(anchorEl)
+  return listItemEl
+}
+
+const buildAncestorsList = function(ancestors) {
+  const listEl = document.createElement("UL");
+  listEl.append(buildAncestorListItem(ancestors[0].name, ancestors[0].id))
+  if (ancestors.length > 1) {
+    listEl.append(buildAncestorListItem(ancestors[1].name, ancestors[1].id))
+  }
+  return listEl
 }
 
 const buildAncestorsEl = function(ancestors) {
-  const ancestorsEl = document.createElement("P")
-  ancestorsEl.append("Lowest Common Ancestor");
-  ancestors.length > 1 ? ancestorsEl.append("s: ") : ancestorsEl.append(": ")
-  ancestorsEl.append(buildAncestorAnchor(ancestors[0].name, ancestors[0].id))
-  if (ancestors.length > 1) {
-    ancestorsEl.append(", ")
-    ancestorsEl.append(buildAncestorAnchor(ancestors[1].name, ancestors[1].id))
-  }
-  console.log(ancestorsEl);
+  const ancestorsEl = document.createElement("P");
+  const ancestorHeader = document.createElement("H6");
+  ancestorHeader.append("Lowest Common Ancestor");
+  if (ancestors.length > 1) ancestorHeader.append("s");
+  ancestorsEl.append(ancestorHeader);
+  ancestorsEl.append(buildAncestorsList(ancestors));
   return ancestorsEl
 }
 
@@ -65,15 +73,22 @@ const formatRelationshipInfo = function(relationshipInfo) {
   } 
 }
 
+const buildRelationshipEl = function(relationshipInfo) {
+  const relationshipEl = document.createElement("P")
+  const relationshipHeader = document.createElement("H6");
+  relationshipHeader.innerText = "Relationship" 
+  relationshipEl.append(relationshipHeader)
+  relationshipEl.append(formatRelationshipInfo(relationshipInfo));
+  return relationshipEl
+}
+
 
 const displayRelationshipInfo = function(relationshipInfo) {
   const relationshipInfoEl = document.getElementById("relationship-info");
   //Clear out the info element
   relationshipInfoEl.textContent = "";
   //Attach the relationship statement (e.g., Relationship: Son)
-  const relationshipEl = document.createElement("P")
-  relationshipEl.innerText = formatRelationshipInfo(relationshipInfo)
-  relationshipInfoEl.appendChild(relationshipEl)
+  relationshipInfoEl.appendChild(buildRelationshipEl(relationshipInfo))
   //If applicable, attach lowest common ancestor statement
   if (relationshipInfo["lowest_common_ancestors"]) {
     const ancestorsEl = buildAncestorsEl(relationshipInfo["lowest_common_ancestors"])
